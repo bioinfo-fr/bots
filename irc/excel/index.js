acbf3d2767c4f56501207dcbdbd9ad423e2e4ae9
Samuel ORTION <samuel.ortion@orange.fr>
2021-05-06 16:29:35 +0200

    Added french i18n

--- irc/excel/index.js
+++ irc/excel/index.js
@@ -19,20 +19,37 @@ let verbose = false
 let sleeping = false
 
 const config = require('./config.json')
+const lang = config.lang
 
 function help(to) {
-    bot.say(to, 'F*cking MS excel IRC Bot help : ')
-    bot.say(to, '  !sleep (⚜) - pause excel bot, set to listen only to !wakeup')
-    bot.say(to, '  !wakeup (⚜) - set excel bot to listen to all other commands')
-    bot.say(to, '  !help - display bot command help')
-    bot.say(to, '  !didusee <pseudo> - say when bot saw <pseudo> in this channel for the last time')
-    bot.say(to, '  !last <pseudo> - say last message said by <pseudo> again')
-    bot.say(to, '  !article [ <query> ] [ -n <number> ] - retrieve article urls and summary from bioinfo-fr.net corresponding to the specified query if any, or last published article(s) if no query is specified')
-    bot.say(to, '  !ideas - set of commands related to new articles ideas for the blog')
-    bot.say(to, '      !ideas list - show all future article ideas')
-    bot.say(to, '      !ideas add <description> - add a new article idea with with specified <description>')
-    bot.say(to, '      !ideas rm <id> - remove idea with specified id')
-    bot.say(to, '  !pubmed [ <query> ] [ -n <number> ] - retrieve url and abstract of <number> (5 by default) recent articles from pubmed')
+    if (lang == 'fr') {
+        bot.say(to, 'F*cking MS excel IRC Bot aide : ')
+        bot.say(to, '  !sleep (⚜) - met en pause excel, qui ne se réveillera qu\' à la commande !wakeup')
+        bot.say(to, '  !wakeup (⚜) - réveille excel')
+        bot.say(to, '  !help - affiche l\'aide des commandes d\'excel')
+        bot.say(to, '  !didusee <pseudo> - affiche la dernière fois où <pseudo> a quitté le chan')
+        bot.say(to, '  !last <pseudo> - redit le dernier message envoyé par <pseudo>')
+        bot.say(to, '  !article [ <query> ] [ -n <number> ] - renvoit 5 url et les résumés des articles correspondant à la requête <query> si elle est spécifiée, le paramètre optionnel -n permet de préciser le nombre d\'article à retourner')
+        bot.say(to, '  !ideas - ensemble de commande pour gérer les idées de nouvel article')
+        bot.say(to, '      !ideas list - retourne la liste des idées')
+        bot.say(to, '      !ideas add <description> - ajoute une nouvelle idée d\'article avec la <description> spécifiée')
+        bot.say(to, '      !ideas rm <id> (⚜) - retire l\'idée associée avec l\'<id> spécifiée')
+        bot.say(to, '  !pubmed [ <query> ] [ -n <number> ] - retourne l`url et l\'abstract de <number> articles depuis pubmed (5 par défaut)')
+    } else {
+        bot.say(to, 'F*cking MS excel IRC Bot help : ')
+        bot.say(to, '  !sleep (⚜) - pause excel bot, set to listen only to !wakeup')
+        bot.say(to, '  !wakeup (⚜) - set excel bot to listen to all other commands')
+        bot.say(to, '  !help - display bot command help')
+        bot.say(to, '  !didusee <pseudo> - say when bot saw <pseudo> in this channel for the last time')
+        bot.say(to, '  !last <pseudo> - say last message said by <pseudo> again')
+        bot.say(to, '  !article [ <query> ] [ -n <number> ] - retrieve article urls and summary from bioinfo-fr.net corresponding to the specified query if any, or last published article(s) if no query is specified')
+        bot.say(to, '  !ideas - set of commands related to new articles ideas for the blog')
+        bot.say(to, '      !ideas list - show all future article ideas')
+        bot.say(to, '      !ideas add <description> - add a new article idea with with specified <description>')
+        bot.say(to, '      !ideas rm <id> - remove idea with specified id')
+        bot.say(to, '  !pubmed [ <query> ] [ -n <number> ] - retrieve url and abstract of <number> (5 by default) recent articles from pubmed')
+
+    }
 }
 
 function addToDb(nick, text) {
